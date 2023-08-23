80e945628c1c2539db446bc6a9a7bb7630df43c9
Winnie Kimani <kimaniwinniew@gmail.com>
2023-08-23 14:46:00 +0300

    add first commit

--- src/main/java/com/systechafrika/App.java
+++ src/main/java/com/systechafrika/App.java
@@ -1,13 +1,16 @@
 package com.systechafrika;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int c, int d) {
+        return c / d;
+    }
+
 }
--- src/test/java/com/systechafrika/AppTest.java
+++ src/test/java/com/systechafrika/AppTest.java
@@ -1,20 +1,31 @@
 package com.systechafrika;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+public class AppTest {
+    // ?Reference =>App
+    App app = new App();
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void add() {
+
+        // when
+        int result = app.add(2, 3);
+        // then
+        int expected = 5;
+        // verify that result=expected
+        Assertions.assertEquals(result, expected, "the sum of 2+3 should be 5");
     }
+
+    @Test
+    void divide() {
+        // when
+        int result1 = app.divide(8, 4);
+        // then
+        int expected2 = 2;
+        // verify that result=expected
+        Assertions.assertEquals(result1, expected2);
+    }
+
 }
