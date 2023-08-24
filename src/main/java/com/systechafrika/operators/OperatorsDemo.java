7d0bea5d716cf144eced2bd8959e6e1024327a5e
Winnie Kimani <kimaniwinniew@gmail.com>
2023-08-24 18:26:52 +0300

    assignment and comparison operators

--- src/main/java/com/systechafrika/operators/OperatorsDemo.java
+++ src/main/java/com/systechafrika/operators/OperatorsDemo.java
@@ -2,8 +2,6 @@ package com.systechafrika.operators;
 
 import java.util.logging.Logger;
 
-import com.systechafrika.variables.VariablesDemo;
-
 public class OperatorsDemo {
     private static final Logger LOGGER = Logger.getLogger(OperatorsDemo.class.getName());
 
@@ -34,9 +32,59 @@ public class OperatorsDemo {
 
     }
 
+    public void assignmentOperators() {
+        // simple assignment operator
+        int a = 5;
+        int sum;
+
+        sum = a;
+
+        // addition assignment operator
+        sum += a;
+        LOGGER.info("the result of the addition assignment is " + sum);
+
+        // subtraction assignment operator
+        sum -= a;
+        LOGGER.info("the result of the subtraction assignment is " + sum);
+
+        // multiplication assignment operator
+        sum *= a;
+        LOGGER.info("the result of the multiplication assignment is " + sum);
+
+        // division assignment operator
+        sum /= 1;
+        LOGGER.info("the result of the division assignment is " + sum);
+
+    }
+
+    public void comparisonOperators() {
+        int x = 5;
+        int y = 3;
+        // greater than operator
+        LOGGER.info("x > y:" + (x > y)); // returns true, because 5 is higher than 3
+
+        // less than operator
+        LOGGER.info("x < y:" + (x < y)); // returns false, because 5 is not less than 3
+
+        // Equal to operator
+        LOGGER.info("x == y:" + (x == y)); // returns false,x is not equal to y
+
+        // Greater than or equal to operator
+        LOGGER.info("x >= y:" + (x >= y)); // returns true
+
+        // less than or equal to operator
+        LOGGER.info("x <= y:" + (x <= y)); // returns false
+
+        // Not equal operator
+        LOGGER.info("x != y:" + (x != y)); // returns true,x is not equal to y
+
+    }
+
     public static void main(String[] args) {
         OperatorsDemo a = new OperatorsDemo();
-        a.arithmeticOperators();
+        // a.arithmeticOperators();
+        // a.assignmentOperators();
+        a.comparisonOperators();
 
     }
 
