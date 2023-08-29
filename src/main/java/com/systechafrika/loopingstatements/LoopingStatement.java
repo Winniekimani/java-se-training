24ea6692d817911e02fcd2dd9c434138b39e8557
Winnie Kimani <kimaniwinniew@gmail.com>
2023-08-29 06:28:17 +0300

    looping statements

---
+++ src/main/java/com/systechafrika/loopingstatements/LoopingStatement.java
@@ -0,0 +1,65 @@
+package com.systechafrika.loopingstatements;
+
+import java.util.logging.Logger;
+
+public class LoopingStatement {
+    private static final Logger LOGGER = Logger.getLogger(LoopingStatement.class.getName());
+
+    public void forLoopStatement() {
+        LOGGER.info("before my loop");
+        for (int index = 0; index < 10; index++) {
+
+            LOGGER.info("MY INDEX IS " + index);
+
+        }
+
+    }
+
+    public void breakJumpStatement() {
+        LOGGER.info("before my loop");
+        for (int index = 0; index < 10; index++) {
+            if (index == 5) {
+                break;
+            }
+            LOGGER.info("MY INDEX IS " + index);
+
+        }
+        LOGGER.info("After my loop ");
+
+    }
+
+    public void continueJumpStatement() {
+        LOGGER.info("before my loop");
+        for (int index = 0; index < 10; index++) {
+            if (index == 5) {
+                continue;
+            }
+            LOGGER.info("MY INDEX IS " + index);
+
+        }
+        LOGGER.info("After my loop ");
+
+    }
+
+    public void returnJumpStatement() {
+        LOGGER.info("before my loop");
+        for (int index = 0; index < 10; index++) {
+            if (index == 4) {
+                return;
+            }
+            LOGGER.info("MY INDEX IS " + index);
+
+        }
+        LOGGER.info("After my loop ");
+
+    }
+
+    public static void main(String[] args) {
+        LoopingStatement a = new LoopingStatement();
+        a.forLoopStatement();
+        // a.continueJumpStatement();
+        // a.breakJumpStatement();
+        // a.returnJumpStatement();
+
+    }
+}
