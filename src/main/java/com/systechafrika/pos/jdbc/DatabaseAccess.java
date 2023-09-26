e0169dee494e2efd4fbd1b944f6d135623d59d97
Winnie Kimani <kimaniwinniew@gmail.com>
2023-09-26 13:29:12 +0300

    chnged the code structure back to mhusikas way

--- src/main/java/com/systechafrika/pos/jdbc/DatabaseAccess.java
+++ src/main/java/com/systechafrika/pos/jdbc/DatabaseAccess.java
@@ -8,11 +8,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
+import com.systechafrika.pos.Item;
 import com.systechafrika.pos.logging.FileLogging;
 
 public class DatabaseAccess {
-
+    private static final Logger LOGGER = Logger.getLogger(DatabaseAccess.class.getName());
     private static final String DB_URL = "jdbc:mysql://localhost:3308/pos";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "javase";
@@ -82,17 +84,42 @@ public class DatabaseAccess {
         System.out.print("Enter unit price: ");
         double unitPrice = scanner.nextDouble();
 
-        String insertQuery = "INSERT INTO items (item_code, quantity, unit_price) VALUES (?, ?, ?);";
-        PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-        preparedStatement.setString(1, itemCode);
-        preparedStatement.setInt(2, quantity);
-        preparedStatement.setDouble(3, unitPrice);
+        // Create an Item object with the entered data
+        Item item = new Item(itemCode, quantity, unitPrice);
 
-        int rowsInserted = preparedStatement.executeUpdate();
-        FileLogging.logInfo(rowsInserted + " row(s) inserted.");
+        // Define the SQL query for insertion
+        String insertQuery = "INSERT INTO items (item_code, quantity, unit_price) VALUES (?, ?, ?);";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+            // Set the values using the Item object
+            preparedStatement.setString(1, item.getItemCode());
+            preparedStatement.setInt(2, item.getQuantity());
+            preparedStatement.setDouble(3, item.getUnitPrice());
+
+            // Execute the insertion query
+            int rowsInserted = preparedStatement.executeUpdate();
+            FileLogging.logInfo(rowsInserted + " row(s) inserted.");
+        } catch (SQLException e) {
+            FileLogging.logError("Error while inserting data: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        // alternatively,we can manually set the values of the placeholders (?) in the
+        // SQL query (insertQuery)
+        // (directly assigning the values)
+        // String insertQuery = "INSERT INTO items (item_code, quantity, unit_price)
+        // VALUES (?, ?, ?);";
+        // PreparedStatement preparedStatement =
+        // connection.prepareStatement(insertQuery);
+        // preparedStatement.setString(1, itemCode);
+        // preparedStatement.setInt(2, quantity);
+        // preparedStatement.setDouble(3, unitPrice);
+
+        // int rowsInserted = preparedStatement.executeUpdate();
+        // FileLogging.logInfo(rowsInserted + " row(s) inserted.");
 
         // Close the PreparedStatement
-        preparedStatement.close();
+        // preparedStatement.close();
     }
 
     private static void retrieveData(Statement statement) throws SQLException {
@@ -104,6 +131,11 @@ public class DatabaseAccess {
             int quantity = resultSet.getInt("quantity");
             double unitPrice = resultSet.getDouble("unit_price");
 
+            // map to object
+            // Item item = new Item(itemCode, quantity, unitPrice);
+            // System.out.println(item);
+
+            // here i am logging log information about each retrieved item to a log file
             FileLogging.logInfo("Item Code: " + itemCode);
             FileLogging.logInfo("Quantity: " + quantity);
             FileLogging.logInfo("Unit Price: " + unitPrice);
